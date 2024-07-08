e736695cd45a44b36d87adb8bd47253683dc24e4
Cyril Beeckman <cyril.beeckman@gmail.com>
2024-07-08 04:58:43 +0200

    our modif

--- server/index.ts
+++ server/index.ts
@@ -6,114 +6,118 @@ import * as console from 'console';
 import { GetCharIdFromStateId } from '@overextended/ox_core/package/server/player/db';
 
 type GetAccountsReponse = {
-  id: Account['id'];
-  label?: Account['label'];
-  group?: Account['group'];
-  balance: Account['balance'];
-  isDefault?: Account['isDefault'];
-  owner: number;
-  type: Account['type'];
-  role: AccountRole;
-  firstName: string;
-  lastName: string;
+	id: Account['id'];
+	label?: Account['label'];
+	group?: Account['group'];
+	balance: Account['balance'];
+	isDefault?: Account['isDefault'];
+	owner: number;
+	type: Account['type'];
+	role: AccountRole;
+	firstName: string;
+	lastName: string;
 };
 
 onClientCallback('ox_banking:getAccounts', async (playerId): Promise<Account[]> => {
-  const player = GetPlayer(playerId);
+	const player = GetPlayer(playerId);
 
-  if (!player) return;
+	if (!player) return;
 
-  const accessAccounts = await oxmysql.rawExecute<GetAccountsReponse[]>(
-    `
+	const accessAccounts = await oxmysql.rawExecute<GetAccountsReponse[]>(
+		`
     SELECT a.id, a.label, a.owner, a.group, a.balance, a.isDefault, a.type, b.firstName, b.lastName, c.role
     FROM \`accounts_access\` c
     LEFT JOIN accounts a ON a.id = c.accountId
     LEFT JOIN characters b ON b.charId = a.owner
     WHERE c.charId = ?
     `,
-    [player.charId]
-  );
+		[player.charId]
+	);
 
-  const accounts: Account[] = accessAccounts.map((account) => ({
-    group: account.group,
-    id: account.id,
-    label: account.label,
-    isDefault: player.charId === account.owner ? account.isDefault : false,
-    balance: account.balance,
-    type: account.type,
-    owner: `${account.firstName} ${account.lastName}`,
-    role: account.role,
-  }));
+	const accounts: Account[] = accessAccounts.map((account) => ({
+		group: account.group,
+		id: account.id,
+		label: account.label,
+		isDefault: player.charId === account.owner ? account.isDefault : false,
+		balance: account.balance,
+		type: account.type,
+		owner: account.firstName ? `${account.firstName} ${account.lastName}` : null,
+		role: account.role,
+	}));
 
-  return accounts;
+	return accounts;
 });
 
 onClientCallback('ox_banking:createAccount', async (playerId, { name, shared }: { name: string; shared: boolean }) => {
-  const { charId } = GetPlayer(playerId);
+	const { charId } = GetPlayer(playerId);
 
-  if (!charId) return;
+	if (!charId) return;
 
-  return await Ox.CreateAccount(charId, name, shared);
+	return await Ox.CreateAccount(charId, name, shared);
 });
 
 onClientCallback('ox_banking:deleteAccount', async (playerId, accountId: number) => {
-  const { charId } = GetPlayer(playerId);
-  const role = charId && (await Ox.GetAccountRole(accountId, charId));
+	const { charId } = GetPlayer(playerId);
+	const role = charId && (await Ox.GetAccountRole(accountId, charId));
 
-  if (role !== 'owner' && role !== 'manager') return;
+	if (role !== 'owner' && role !== 'manager') return;
 
-  return await Ox.DeleteAccount(accountId);
+	return await Ox.DeleteAccount(accountId);
 });
 
 interface UpdateBalance {
-  accountId: number;
-  amount: number;
+	accountId: number;
+	amount: number;
 }
 
 interface TransferBalance {
-  fromAccountId: number;
-  target: string | number;
-  transferType: 'account' | 'person';
-  amount: number;
+	fromAccountId: number;
+	target: string | number;
+	transferType: 'account' | 'person';
+	amount: number;
 }
 
 onClientCallback('ox_banking:depositMoney', async (playerId, { accountId, amount }: UpdateBalance) => {
-  return await Ox.DepositMoney(playerId, accountId, amount);
+	return await Ox.DepositMoney(playerId, accountId, amount);
 });
 
 onClientCallback('ox_banking:withdrawMoney', async (playerId, { accountId, amount }: UpdateBalance) => {
-  return await Ox.WithdrawMoney(playerId, accountId, amount);
+	return await Ox.WithdrawMoney(playerId, accountId, amount);
 });
 
 onClientCallback(
-  'ox_banking:transferMoney',
-  async (playerId, { fromAccountId, target, transferType, amount }: TransferBalance) => {
-    const { charId } = GetPlayer(playerId);
-    const role = charId && (await Ox.GetAccountRole(fromAccountId, charId));
+	'ox_banking:transferMoney',
+	async (playerId, { fromAccountId, target, transferType, amount }: TransferBalance) => {
+		const { charId } = GetPlayer(playerId);
+		const role = charId && (await Ox.GetAccountRole(fromAccountId, charId));
 
-    if (role !== 'owner' && role !== 'manager') return;
+		if (role !== 'owner' && role !== 'manager') return;
 
-    const targetAccountId =
-      transferType === 'account' ? (target as number) : (await Ox.GetCharacterAccount(target))?.id;
+		const targetAccountId =
+			transferType === 'account' ? (target as number) : (await Ox.GetCharacterAccount(target))?.id;
 
-    if (targetAccountId) {
-      //@todo notify
-      return await Ox.TransferAccountBalance(fromAccountId, targetAccountId, amount);
-    }
-  }
+		if (targetAccountId) {
+			//@todo notify
+			return await Ox.TransferAccountBalance({
+				fromId: fromAccountId,
+				toId: targetAccountId,
+				amount: amount
+			});
+		}
+	}
 );
 
 onClientCallback('ox_banking:getDashboardData', async (playerId): Promise<DashboardData> => {
-  const account = await GetPlayer(playerId)?.getAccount();
+	const account = await GetPlayer(playerId)?.getAccount();
 
-  if (!account) return;
+	if (!account) return;
 
-  const overview = await oxmysql.rawExecute<{
-    day: string;
-    income: number;
-    expenses: number;
-  }[]>(
-    `
+	const overview = await oxmysql.rawExecute<{
+		day: string;
+		income: number;
+		expenses: number;
+	}[]>(
+		`
     SELECT
       DAYNAME(d.date) as day,
       COALESCE(SUM(CASE WHEN at.toId = ? THEN at.amount ELSE 0 END), 0) as income,
@@ -131,59 +135,59 @@ onClientCallback('ox_banking:getDashboardData', async (playerId): Promise<Dashbo
     GROUP BY d.date
     ORDER BY d.date ASC
     `,
-    [account.id, account.id, account.id, account.id]
-  );
+		[account.id, account.id, account.id, account.id]
+	);
 
-  const lastTransactions = await oxmysql.rawExecute<{
-    amount: number;
-    date: string;
-    toId?: number;
-    fromId?: number;
-    message?: string;
-  }[]>(
-    `
+	const lastTransactions = await oxmysql.rawExecute<{
+		amount: number;
+		date: string;
+		toId?: number;
+		fromId?: number;
+		message?: string;
+	}[]>(
+		`
     SELECT amount, date, toId, fromId, message
     FROM accounts_transactions
     WHERE toId = ? OR fromId = ?
     ORDER BY date DESC
     LIMIT 5
     `,
-    [account.id, account.id]
-  );
+		[account.id, account.id]
+	);
 
-  const transactions: Transaction[] = lastTransactions.map((transaction) => {
-    return {
-      amount: transaction.amount,
-      date: transaction.date,
-      message: transaction.message,
-      type: transaction.toId === account.id ? 'inbound' : 'outbound',
-    };
-  });
+	const transactions: Transaction[] = lastTransactions.map((transaction) => {
+		return {
+			amount: transaction.amount,
+			date: transaction.date,
+			message: transaction.message,
+			type: transaction.toId === account.id ? 'inbound' : 'outbound',
+		};
+	});
 
-  return {
-    balance: account.balance,
-    overview,
-    transactions,
-    invoices: [],
-  };
+	return {
+		balance: account.balance,
+		overview,
+		transactions,
+		invoices: [],
+	};
 });
 
 onClientCallback(
-  'ox_banking:getAccountUsers',
-  async (
-    playerId,
-    data: {
-      accountId: number;
-      page: number;
-      search?: string;
-    }
-  ): Promise<AccessTableData> => {
-    const { accountId, page, search } = data;
+	'ox_banking:getAccountUsers',
+	async (
+		playerId,
+		data: {
+			accountId: number;
+			page: number;
+			search?: string;
+		}
+	): Promise<AccessTableData> => {
+		const { accountId, page, search } = data;
 
-    const wildcard = `%${search}%`;
+		const wildcard = `%${search}%`;
 
-    const users = await oxmysql.rawExecute<AccessTableData['users']>(
-      `
+		const users = await oxmysql.rawExecute<AccessTableData['users']>(
+			`
       SELECT c.stateId, a.role, CONCAT(c.firstName, " ", c.lastName) AS \`name\` FROM \`accounts_access\` a
       LEFT JOIN \`characters\` c ON c.charId = a.charId
       WHERE a.accountId = ?
@@ -192,108 +196,108 @@ onClientCallback(
       LIMIT 7
       OFFSET ?
       `,
-      [accountId, wildcard, page * 7]
-    );
+			[accountId, wildcard, page * 7]
+		);
 
-    const usersCount = await oxmysql.prepare<number>(
-      'SELECT COUNT(*) FROM `accounts_access` ac LEFT JOIN characters c ON c.charId = ac.charId WHERE accountId = ? AND CONCAT(c.firstName, " ", c.lastName) LIKE ?',
-      [accountId, wildcard]
-    );
+		const usersCount = await oxmysql.prepare<number>(
+			'SELECT COUNT(*) FROM `accounts_access` ac LEFT JOIN characters c ON c.charId = ac.charId WHERE accountId = ? AND CONCAT(c.firstName, " ", c.lastName) LIKE ?',
+			[accountId, wildcard]
+		);
 
-    return {
-      numberOfPages: Math.ceil(usersCount / 7),
-      users,
-    };
-  }
+		return {
+			numberOfPages: Math.ceil(usersCount / 7),
+			users,
+		};
+	}
 );
 
 onClientCallback(
-  'ox_banking:addUserToAccount',
-  async (
-    playerId,
-    data: {
-      accountId: number;
-      stateId: string;
-      role: string;
-    }
-  ) => {
-    const { accountId, stateId, role } = data;
-    const { charId } = GetPlayer(playerId);
-    const userRole = await Ox.GetAccountRole(accountId, charId);
+	'ox_banking:addUserToAccount',
+	async (
+		playerId,
+		data: {
+			accountId: number;
+			stateId: string;
+			role: string;
+		}
+	) => {
+		const { accountId, stateId, role } = data;
+		const { charId } = GetPlayer(playerId);
+		const userRole = await Ox.GetAccountRole(accountId, charId);
 
-    if (userRole !== 'owner' && userRole !== 'manager') return;
+		if (userRole !== 'owner' && userRole !== 'manager') return;
 
-    const success = await oxmysql.prepare('SELECT 1 FROM `characters` WHERE `stateId` = ?', [stateId]);
+		const success = await oxmysql.prepare('SELECT 1 FROM `characters` WHERE `stateId` = ?', [stateId]);
 
-    // todo locale
-    if (!success) return 'No person with provided state id found.';
+		// todo locale
+		if (!success) return 'No person with provided state id found.';
 
-    return await Ox.SetAccountAccess(accountId, charId, role);
-  }
+		return await Ox.SetAccountAccess(accountId, charId, role);
+	}
 );
 
 onClientCallback(
-  'ox_banking:manageUser',
-  async (
-    playerId,
-    data: {
-      accountId: number;
-      targetStateId: string;
-      values: { role: string };
-    }
-  ): Promise<boolean> => {
-    const { charId } = GetPlayer(playerId);
-    const role = await Ox.GetAccountRole(data.accountId, charId);
+	'ox_banking:manageUser',
+	async (
+		playerId,
+		data: {
+			accountId: number;
+			targetStateId: string;
+			values: { role: string };
+		}
+	): Promise<boolean> => {
+		const { charId } = GetPlayer(playerId);
+		const role = await Ox.GetAccountRole(data.accountId, charId);
 
-    if (role !== 'owner') return false;
+		if (role !== 'owner') return false;
 
-    return (await Ox.SetAccountAccess(data.accountId, data.targetStateId, data.values.role)) > 0;
-  }
+		return (await Ox.SetAccountAccess(data.accountId, data.targetStateId, data.values.role)) > 0;
+	}
 );
 
 onClientCallback('ox_banking:removeUser', async (playerId, data: { targetStateId: string; accountId: number }) => {
-  const { charId } = GetPlayer(playerId);
-  const role = await Ox.GetAccountRole(data.accountId, charId);
+	const { charId } = GetPlayer(playerId);
+	const role = await Ox.GetAccountRole(data.accountId, charId);
 
-  if (role !== 'owner' && role !== 'manager') return;
+	if (role !== 'owner' && role !== 'manager') return;
 
-  return await Ox.RemoveAccountAccess(data.accountId, data.targetStateId);
+	return await Ox.RemoveAccountAccess(data.accountId, data.targetStateId);
 });
 
 onClientCallback(
-  'ox_banking:transferOwnership',
-  async (
-    playerId,
-    data: {
-      targetStateId: string;
-      accountId: number;
-    }
-  ): Promise<true | 'state_id_not_exists'> => {
-    // todo: move to ox_core
+	'ox_banking:transferOwnership',
+	async (
+		playerId,
+		data: {
+			targetStateId: string;
+			accountId: number;
+		}
+	): Promise<true | 'state_id_not_exists'> => {
+		// todo: move to ox_core
 
-    const { charId } = GetPlayer(playerId);
-    const accountRole = await Ox.GetAccountRole(data.accountId, charId);
+		const { charId } = GetPlayer(playerId);
+		const accountRole = await Ox.GetAccountRole(data.accountId, charId);
 
-    if (accountRole !== 'owner') return;
+		if (accountRole !== 'owner') return;
 
-    const targetCharId = await oxmysql.prepare<number | null>('SELECT `charId` FROM `characters` WHERE `stateId` = ?', [
-      data.targetStateId,
-    ]);
+		const targetCharId = await oxmysql.prepare<number | null>('SELECT `charId` FROM `characters` WHERE `stateId` = ?', [
+			data.targetStateId,
+		]);
 
-    if (!targetCharId) return 'state_id_not_exists';
+		if (!targetCharId) return 'state_id_not_exists';
 
-    await oxmysql.prepare(
-      "INSERT INTO `accounts_access` (`accountId`, `charId`, `role`) VALUES (?, ?, 'owner') ON DUPLICATE KEY UPDATE `role` = 'owner'",
-      [data.accountId, targetCharId]
-    );
+		await oxmysql.prepare(
+			"INSERT INTO `accounts_access` (`accountId`, `charId`, `role`) VALUES (?, ?, 'owner') ON DUPLICATE KEY UPDATE `role` = 'owner'",
+			[data.accountId, targetCharId]
+		);
 
-    await oxmysql.prepare('UPDATE `accounts` SET `owner` = ? WHERE `id` = ?', [targetCharId, data.accountId]);
+		await oxmysql.prepare('UPDATE `accounts` SET `owner` = ? WHERE `id` = ?', [targetCharId, data.accountId]);
 
-    await oxmysql.prepare("UPDATE `accounts_access` SET `role` = 'manager' WHERE `accountId` = ? AND `charId` = ?", [
-      data.accountId,
-      charId,
-    ]);
+		await oxmysql.prepare("UPDATE `accounts_access` SET `role` = 'manager' WHERE `accountId` = ? AND `charId` = ?", [
+			data.accountId,
+			charId,
+		]);
 
-    return true;
-  }
+		return true;
+	}
 );
--- web/src/layouts/bank/pages/accounts/components/AccountDetails.tsx
+++ web/src/layouts/bank/pages/accounts/components/AccountDetails.tsx
@@ -11,77 +11,80 @@ import locales from '@/locales';
 import TransferModal from '@/layouts/bank/pages/accounts/modals/TransferModal';
 
 const AccountDetails: React.FC = () => {
-  const modal = useModal();
-  const account = useActiveAccount()!;
+	const modal = useModal();
+	const account = useActiveAccount()!;
 
-  return (
-    <BaseCard title="Details" icon={ScanText} className="flex-1">
-      <div className="flex justify-between">
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col">
-            <p className="text-xs text-muted-foreground">{locales.account_name}</p>
-            <p>{account.label}</p>
-          </div>
-          <div className="flex flex-col">
-            <p className="text-xs text-muted-foreground">{locales.account_type}</p>
-            <p>{locales.personal_account}</p>
-          </div>
-        </div>
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col">
-            <p className="text-xs text-muted-foreground">{locales.account_number}</p>
-            <div className="flex items-center gap-2">
-              <p>{account.id}</p>
-              <Tooltip delayDuration={200}>
-                <TooltipTrigger asChild>
-                  <button className="flex items-center">
-                    <Copy size={14} />
-                  </button>
-                </TooltipTrigger>
-                <TooltipContent>{locales.copy}</TooltipContent>
-              </Tooltip>
-            </div>
-          </div>
-          <div className="flex flex-col">
-            <p className="text-xs text-muted-foreground">{locales.disposable_amount}</p>
-            <p>{formatNumber(account.balance)}</p>
-          </div>
-        </div>
-        <div className="flex flex-col">
-          <p className="text-xs text-muted-foreground">{locales.account_owner}</p>
-          <p>{account.owner}</p>
-        </div>
-      </div>
-      <div className="flex h-full flex-col gap-2">
-        <AccountButton
-          label={locales.withdraw}
-          icon={Wallet}
-          onClick={() =>
-            modal.open({
-              title: locales.withdraw,
-              children: <DepositWithdrawModal account={account} />,
-            })
-          }
-        />
-        <AccountButton
-          label={locales.deposit}
-          icon={Landmark}
-          onClick={() =>
-            modal.open({
-              title: locales.deposit,
-              children: <DepositWithdrawModal account={account} isDeposit={true} />,
-            })
-          }
-        />
-        <AccountButton
-          label={locales.transfer}
-          icon={Repeat}
-          onClick={() => modal.open({ title: locales.transfer, children: <TransferModal account={account} /> })}
-        />
-        <AccountButton label={locales.logs} icon={History} />
-      </div>
-    </BaseCard>
-  );
+	return (
+		<BaseCard title="Details" icon={ScanText} className="flex-1">
+			<div className="flex justify-between">
+				<div className="flex flex-col gap-4">
+					<div className="flex flex-col">
+						<p className="text-xs text-muted-foreground">{locales.account_name}</p>
+						<p>{account.label}</p>
+					</div>
+					<div className="flex flex-col">
+						<p className="text-xs text-muted-foreground">{locales.account_type}</p>
+						<p>{locales.personal_account}</p>
+					</div>
+				</div>
+				<div className="flex flex-col gap-4">
+					<div className="flex flex-col">
+						<p className="text-xs text-muted-foreground">{locales.account_number}</p>
+						<div className="flex items-center gap-2">
+							<p>{account.id}</p>
+							<Tooltip delayDuration={200}>
+								<TooltipTrigger asChild>
+									<button className="flex items-center">
+										<Copy size={14} />
+									</button>
+								</TooltipTrigger>
+								<TooltipContent>{locales.copy}</TooltipContent>
+							</Tooltip>
+						</div>
+					</div>
+					<div className="flex flex-col">
+						<p className="text-xs text-muted-foreground">{locales.disposable_amount}</p>
+						<p>{formatNumber(account.balance)}</p>
+					</div>
+				</div>
+				{account.owner && (
+					<div className="flex flex-col">
+						<p className="text-xs text-muted-foreground">{locales.account_owner}</p>
+						<p>{account.owner}</p>
+					</div>
+				)}
+
+			</div>
+			<div className="flex h-full flex-col gap-2">
+				<AccountButton
+					label={locales.withdraw}
+					icon={Wallet}
+					onClick={() =>
+						modal.open({
+							title: locales.withdraw,
+							children: <DepositWithdrawModal account={account} />,
+						})
+					}
+				/>
+				<AccountButton
+					label={locales.deposit}
+					icon={Landmark}
+					onClick={() =>
+						modal.open({
+							title: locales.deposit,
+							children: <DepositWithdrawModal account={account} isDeposit={true} />,
+						})
+					}
+				/>
+				<AccountButton
+					label={locales.transfer}
+					icon={Repeat}
+					onClick={() => modal.open({ title: locales.transfer, children: <TransferModal account={account} /> })}
+				/>
+				<AccountButton disabled={true} label={locales.logs} icon={History} />
+			</div>
+		</BaseCard>
+	);
 };
 
 export default AccountDetails;
